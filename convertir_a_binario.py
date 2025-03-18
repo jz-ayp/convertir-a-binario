fca1fa701ae3fc1d03d9b4fec824b77c90c65cb2
jzaldivar <jorge.zaldivar@gmail.com>
2025-03-17 20:05:32 -0600

    Code "real" solution

--- convertir_a_binario.py
+++ convertir_a_binario.py
@@ -1,15 +1,22 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+Convertir un número decimal (base 10) a binario.
 """
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+numero = int(input("Introduzca el número a convertir: "))
 
 # Proceso
-
+cociente = numero
+if numero == 0:
+    binario = "0"
+else:
+    binario = ""
+    # Hacer divisiones sucesivas hasta que el cociente de 0
+    while cociente > 0:
+        residuo = cociente % 2
+        cociente = cociente // 2
+        # Agregar el nuevo residuo a los anteriores (pero "al revés")
+        binario = str(residuo) + binario 
 
 # Salidas
-print(salida)
+print(f"El número en binario es {binario}.")
